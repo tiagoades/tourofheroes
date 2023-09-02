e0965715fa07ed2431e357948bea5490136cbdde
tiagoades <tiago.almeida.s@gmail.com>
2023-09-02 00:07:32 -0300

    ANGULAR MATERIAL

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Homem Aranha' },
+  { id: 3, name: 'Dr. Estranho' },
+  { id: 4, name: 'Wanda' },
+  { id: 5, name: 'Pantera Negra' },
+  { id: 6, name: 'Capitã Marvel' },
+  { id: 7, name: 'Homem de Ferro' },
+  { id: 8, name: 'Hulk' },
+  { id: 9, name: 'Capitão America' },
+  { id: 10, name: 'Viuva Negra' },
+  { id: 11, name: 'Gavião Arqueiro' },
+  { id: 12, name: 'Homem Formiga' },
+  { id: 13, name: 'Vespa' },
+  { id: 14, name: 'Rock' },
+  { id: 15, name: 'Sr. das Estrelas' },
+]
