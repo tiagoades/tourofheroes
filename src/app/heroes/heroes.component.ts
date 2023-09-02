5af078da122610c02ecf762020ea8aa100bfbf68
tiagoades <tiago.almeida.s@gmail.com>
2023-09-02 00:05:38 -0300

    ANGULAR MATERIAL

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,14 @@ import { Hero } from '../hero.model';
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
+
+  }
 
 }
