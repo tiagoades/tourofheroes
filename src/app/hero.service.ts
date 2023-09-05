60ab2bec80cf3482f6327572c6392a90d146ddb0
tiagoades <tiago.almeida.s@gmail.com>
2023-09-05 19:23:43 -0300

    OBSERVABLE

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero.model';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[]{
+  getHeroes(): Observable<Hero[]>{
 
-    return HEROES;
+    const heroes = of(HEROES);
+
+    return heroes;
 
   }
 
