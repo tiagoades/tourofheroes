8b0107211c071ad3fb422150e31c4aaaaa298365
tiagoades <tiago.almeida.s@gmail.com>
2023-09-09 23:58:51 -0300

    MATERIAL MODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatIconModule} from '@angular/material/icon';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatTooltipModule} from '@angular/material/tooltip';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {MatCardModule} from '@angular/material/card';
-
+import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,14 +22,10 @@ import {MatCardModule} from '@angular/material/card';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatTooltipModule,
     FlexLayoutModule,
     AppRoutingModule,
-    MatCardModule
+    MaterialModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
