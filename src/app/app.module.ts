2b9cda33f4fe02864e77ac4131efd37097bb41f3
tiagoades <tiago.almeida.s@gmail.com>
2023-09-13 17:42:25 -0300

    FEATURE MODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    DashboardComponent,
   ],
   imports: [
+    // @angular
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+
+    //feature
+    DashboardModule,
+    HeroesModule,
+
+    // app
     FlexLayoutModule,
     AppRoutingModule,
     CoreModule,
